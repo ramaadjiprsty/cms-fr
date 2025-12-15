29af4d2533f92fdee518ae3df1ef00241b6d6fab
Rama Adji Prasetyo <ramaadjiprsty@gmail.com>
2025-12-15 13:30:51 +0700

    initial setup prisma logging and global module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient, Prisma } from 'generated/prisma/client';
+import { InjectPinoLogger, PinoLogger } from 'pino-nestjs';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(
+    @InjectPinoLogger(PrismaService.name) private readonly logger: PinoLogger,
+  ) {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    super({
+      log: isProduction
+        ? [
+            { emit: 'event', level: 'error' },
+            { emit: 'event', level: 'warn' },
+          ]
+        : [
+            { emit: 'event', level: 'query' },
+            { emit: 'event', level: 'info' },
+            { emit: 'event', level: 'warn' },
+            { emit: 'event', level: 'error' },
+          ],
+    } as Prisma.PrismaClientOptions);
+  }
+
+  async onModuleInit() {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    if (!isProduction) {
+      (this as any).$on('query', (e) => {
+        this.logger.info(e.query);
+      });
+      (this as any).$on('info', (e) => {
+        this.logger.info(e);
+      });
+    }
+
+    (this as any).$on('error', (e) => {
+      this.logger.error(e);
+    });
+    (this as any).$on('warn', (e) => {
+      this.logger.warn(e);
+    });
+
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
