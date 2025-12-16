b948565a25c5c337b603958523648a7e728fc5fc
Rama Adji Prasetyo <ramaadjiprsty@gmail.com>
2025-12-16 15:32:06 +0700

    feat: update getHello method to return detailed service information as an object

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object {
+    return {
+      app: 'CMS API Service',
+      version: '1.0.0',
+      status: 'Running',
+      author: 'Rama Adji',
+      documentation: 'https://ramaadjiprsty.my.id/documentation',
+      serverTime: new Date().toISOString(),
+    };
   }
 }
