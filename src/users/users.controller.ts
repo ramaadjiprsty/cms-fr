ed79eed6cb3c5f0d916774c408743eec09abe4a9
Rama Adji Prasetyo <ramaadjiprsty@gmail.com>
2025-12-16 10:25:06 +0700

    feat(users): implement search, pagination, and CRUD operations
    - Add `QueryUserDto` to handle search queries and pagination.
    - Implement `findAll` method with filtering support.
    - Implement `findById` for single user retrieval.
    - Implement `remove` method for user deletion.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { UserEntity } from './entities/user.entity';
 import { ApiOkResponse } from '@nestjs/swagger';
+import { InjectPinoLogger, PinoLogger } from 'pino-nestjs';
+import { QueryUserDto } from './dto/query-user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    @InjectPinoLogger(UsersController.name) private logger: PinoLogger,
+  ) {}
 
   @Post('register')
-  @ApiOkResponse({type: UserEntity})
+  @ApiOkResponse({ type: UserEntity })
   async register(@Body() dto: CreateUserDto): Promise<UserEntity> {
+    this.logger.info(`User with email ${dto.email} registered`);
     return new UserEntity(await this.usersService.create(dto));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UserEntity } from './entities/user.entity';
+import { QueryUserDto } from './dto/query-user.dto';
+import { Prisma } from 'generated/client';
 
 export const roundOfHashing = 10;
 
@@ -23,5 +25,113 @@ export class UsersService {
     }
 
     return new UserEntity(await this.prismaService.user.create({ data: dto }));
+  }
+
+  async findAll(): Promise<UserEntity[]> {
+    const users = await this.prismaService.user.findMany();
+
+    if (!users) {
+      throw new HttpException('No users found', 404);
+    }
+
+    return users.map((user) => new UserEntity(user));
+  }
+
+  // async findByEmail(email: string): Promise<UserEntity> {
+  //   const user = await this.prismaService.user.findUnique({
+  //     where: { email },
+  //   });
+
+  //   if (!user) {
+  //     throw new HttpException(`User with email ${email} not found`, 404);
+  //   }
+
+  //   return new UserEntity(user);
+  // }
+
+  async findById(id: string): Promise<UserEntity> {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(`User with ID ${id} not found`, 404);
+    }
+
+    return new UserEntity(user);
+  }
+
+  async remove(id: string): Promise<UserEntity> {
+    const user = await this.findById(id);
+
+    if (!user) {
+      throw new HttpException(`User with ID ${id} not found`, 404);
+    }
+
+    await this.prismaService.user.delete({
+      where: {
+        id,
+      },
+    });
+
+    return new UserEntity(user);
+  }
+
+  async search(
+    query: QueryUserDto,
+  ): Promise<{ metadata: any; data: UserEntity[] }> {
+    const { search, page, limit, sortBy, sortOrder } = query;
+    const skip = (page - 1) * limit;
+
+    const where: Prisma.UserWhereInput = {};
+
+    if (search) {
+      where.OR = [
+        {
+          firstName: {
+            contains: search,
+            mode: 'insensitive',
+          },
+        },
+        {
+          lastName: {
+            contains: search,
+            mode: 'insensitive',
+          },
+        },
+        {
+          email: {
+            contains: search,
+            mode: 'insensitive',
+          },
+        },
+      ];
+    }
+
+    const orderBy: Prisma.UserOrderByWithRelationInput = {
+      [sortBy]: sortOrder,
+    };
+
+    const [users, total] = await this.prismaService.$transaction([
+      this.prismaService.user.findMany({
+        where,
+        orderBy,
+        skip,
+        take: limit,
+      }),
+      this.prismaService.user.count({ where }),
+    ]);
+
+    return {
+      metadata: {
+        total,
+        page,
+        limit,
+        lastPage: Math.ceil(total / limit),
+      },
+      data: users.map((user) => new UserEntity(user)),
+    };
   }
 }
